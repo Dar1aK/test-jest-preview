d4158e7402063a36c8b20b7261d7b378a7bfe75d
Daria Karataeva <dkarataeva@samokat.ru>
2025-11-24 21:35:25 +0300

    fix transform config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  transform: {
+    "^.+\\.(css|scss|sass|less)$": "jest-preview/transforms/css",
+    "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)":
+      "jest-preview/transforms/file",
+  },
 };
